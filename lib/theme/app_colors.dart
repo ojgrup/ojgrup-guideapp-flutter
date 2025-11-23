3c8cbe4b8884dac1e11c736bfc62913cdf75f54a
ojgrup <108789772+ojgrup@users.noreply.github.com>
2025-11-24 00:51:14 +0800

    Create app_colors.dart

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// Kelas untuk menyimpan definisi warna aplikasi
+class AppColors {
+  // Hijau Utama (Digunakan untuk tombol, app bar, ikon)
+  static const Color primaryGreen = Color(0xFF4CAF50); // Material Green 500
+  
+  // Hijau Gelap (Digunakan untuk teks judul/bold)
+  static const Color darkGreen = Color(0xFF1B5E20); // Material Green 900
+  
+  // Warna Putih Polos (Digunakan untuk background)
+  static const Color primaryWhite = Colors.white;
+}
